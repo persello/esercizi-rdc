4cb14d6ed611ea2cf02dbdb7b4eeec8849625e51
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-06-10 01:52:37 +0200

    Server update.

--- Esercizi/NSD01/1_asta_telematica/server.c
+++ Esercizi/NSD01/1_asta_telematica/server.c
@@ -9,36 +9,41 @@
 #define PORT 25000
 #define STARTING_PRICE 0.0
 
-int main(int argc, char *argv[])
+void server_loop(int socket_handle)
 {
-    int socket_handle;
     char buffer[BUFSIZ + 1];
+
+    // Offerte
     double current_offer = STARTING_PRICE;
     double max_offer = STARTING_PRICE;
+
+    // Nomi
     char current_offer_name[32];
     char max_offer_name[32];
 
+    // Preparazione delle stringhe
     strcpy(current_offer_name, "");
     strcpy(max_offer_name, "");
 
-    printf("Avvio server asta telematica.\n");
-    socket_handle = create_udp_server("0.0.0.0", PORT);
-
-    if (socket_handle > -1)
-    {
-        printf("Creato un server UDP con socket identifier: %d.\n", socket_handle);
-    }
-    else
-    {
-        printf("Errore durante la creazione del server.\n");
-    }
-
+    // Bloccante
     while (udp_receive(socket_handle, buffer) > 0)
     {
+
+        /**
+         * Se il comando è valido, in current_offer_name ci sarà il nome dell'offerente,
+         * mentre in current_offer l'offerta corrente.
+         * Se non lo è, lo si indica al mittente.
+         * 
+         */
         if (parse_command(buffer, current_offer_name, &current_offer))
         {
             printf("Offerta ricevuta di %f da %s.\n", current_offer, current_offer_name);
 
+            /**
+             * Se l'offerta corrente è la migliore salvo offerta e nome (per eventuali usi successivi).
+             * Se non lo è, si invia la risposta senza memorizzare nulla.
+             * 
+             */
             if (current_offer > max_offer)
             {
                 printf("Offerta accettata.\n");
@@ -61,6 +66,27 @@ int main(int argc, char *argv[])
             try_reply(socket_handle, "INVALIDA");
         }
     }
+}
 
+int main(int argc, char *argv[])
+{
+    int socket_handle;
+
+    // Avvio del server
+    printf("Avvio server asta telematica.\n");
+    if ((socket_handle = create_udp_server("0.0.0.0", PORT)) > -1)
+    {
+        printf("Creato un server UDP con socket identifier: %d.\n", socket_handle);
+    }
+    else
+    {
+        printf("Errore durante la creazione del server.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Programma interattivo
+    server_loop(socket_handle);
+
+    // Chiusura
     die(socket_handle, EXIT_SUCCESS);
 }
